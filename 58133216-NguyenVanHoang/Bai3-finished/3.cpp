af2b2d0c86d65badd5496346376e2c3481ed1e4a
Hoang Nguyen <nvanhoang188@gmail.com>
2018-12-27 13:20:02 +0700

    fix bug

--- 58133216-NguyenVanHoang/Bai3-finished/3.cpp
+++ 58133216-NguyenVanHoang/Bai3-finished/3.cpp
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <vector>
 #include <fstream>
+#include <stdlib.h>
+#include <windows.h>
 
 using namespace std;
 
@@ -12,58 +14,70 @@ struct Employee {
     string lastName; // ho
     int salary;
 } empF, empS; // employee finded, employee have max of salary
-
 class empList {
-    private:
-        int n; //numbers of employee;
     public:
+        int n;
     	vector<Employee> ds;
-        void enterEmpList();
+        void enterEmps();
         void showEmpList();
         Employee findEmp();
         Employee maxOfSalary();
         void sort();
 };
 empList emps;
-int mo; // menu option
-char rtnMenu; // return to menu
+int option;
+char rtnmenu;
 
-void readInputFile();
+void readFile();
 void showMenu();
 void menuOption();
-void enterEmps();
 void writeToInputFile(empList emps);
 void writeToOutputFile(Employee empF, Employee empS, empList emps);
-bool returnToMenu();
+bool returnMenu();
+void goTo(int x, int y) {
+    COORD co = {x,y};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), co);
+}
 
 int main() {
+    readFile();
     showMenu();
     menuOption();
 }
 
-void empList::enterEmpList() {
-    cout << "Enter quantity: "; cin >> n;
+void empList::enterEmps() {
+    int tmp;
+    cout << "Enter quantity: "; cin >> tmp;
+    int m = n;
+    n += tmp;
     ds.resize(n);
-    for(int i = 0; i < n; i++) {
-        cout << "======================" << endl;
+    // cout << m << " " << n << endl;
+    for(int i = m; i < n; i++) {
         cout << "Employee code: ";
         cin >> ds[i].empCode;
-        cout << "\nFirst name: ";
+
+        cout << "First name: ";
         fflush(stdin);
         getline(cin, ds[i].firstName);
-        cout << "\nLast name: ";
+
+        cout << "Last name: ";
         fflush(stdin);
         getline(cin, ds[i].lastName);
-        cout << "\nSalary: ";
+
+        cout << "Salary: ";
         cin >> ds[i].salary;
     }
 }
 void empList::showEmpList() {
     if(n) {
-        cout << "STT\t" << "Emp code\t" << "Ho ten\t\t\t" << "Salary" << endl;
+        cout << "STT\t" << "Emp code\t" << "Ho ten";
+        goTo(50, 1);
+        cout << "Salary" << endl;
         for(int i = 0; i < n; i++) {
             cout << i + 1 << "\t" << ds[i].empCode << "\t\t";
-            cout << ds[i].lastName << " " << ds[i].firstName << "\t\t" << ds[i].salary << endl;
+            cout << ds[i].lastName << " " << ds[i].firstName;
+            goTo(50, 2 + i);
+            cout << ds[i].salary << endl;
         }
     } else {
         cout << "======================" << endl;
@@ -72,15 +86,12 @@ void empList::showEmpList() {
 }
 Employee empList::findEmp() {
     int empCode;
-    cout << "Enter employee code:> "; cin >> empCode;
-
-    bool flag = false;
+    cout << "Enter employee code: "; cin >> empCode;
     for(int i = 0; i < n; i++) {
-        if(ds[i].empCode == empCode) {
+        if(ds[i].empCode == empCode)
             return ds[i];
-        }
     }
-    return empF;
+    return empF; // not found
 }
 Employee empList::maxOfSalary() {
     if(ds.size()) {
@@ -91,8 +102,7 @@ Employee empList::maxOfSalary() {
                 max = ds[i].salary;
                 key = i;
             }
-        }
-        return ds[key];
+        } return ds[key];
     } else {
         return empS;
     }
@@ -101,8 +111,6 @@ void empList::sort() {
     for(int i = 0; i < n - 1; i++)
         for(int j = i + 1; j < n; j++) {
             if(ds[i].firstName.compare(ds[j].firstName) > 0) {
-                // if ds[i].firstName > ds[i + 1].firstName
-                // ASCII greater
                 Employee tmp = ds[i];
                 ds[i] = ds[j];
                 ds[j] = tmp;
@@ -115,24 +123,60 @@ void empList::sort() {
             }
         }
 }
+void readFile() {
+    ifstream myfile;
+    myfile.open("input3.txt");
+    
+    if(myfile.is_open()) {
+        string line;
+        int i = 0;
+        int tmp;
+        while(getline(myfile, line)) {
+            tmp = i / 4;
+            emps.ds.resize(tmp + 1);
+            switch(i % 4) {
+                case 0:
+                    emps.ds[tmp].empCode = atoi(line.c_str());
+                    i++;
+                    break;
+                case 1:
+                    emps.ds[tmp].lastName = line;
+                    i++;
+                    break;
+                case 2:
+                    emps.ds[tmp].firstName = line;
+                    i++;
+                    break;
+                case 3:
+                    emps.ds[tmp].salary = atoi(line.c_str());
+                    i++;
+                    break;
+            }
+        } emps.n = tmp + 1;
+    } else {
+        // cout << "Unable open to file" << endl;
+    }
+
+    myfile.close();
+}
 void showMenu() {
     cout << "\n\n";
-    cout << "\t\t\t\t1. Enter employees" << endl;
+    cout << "\t\t\t\t1. Add employees" << endl;
     cout << "\t\t\t\t2. Find employee" << endl;
     cout << "\t\t\t\t3. Max of salary" << endl;
-    cout << "\t\t\t\t4. Sort employee list by name" << endl;
-    cout << "\t\t\t\t5. Save to output file" << endl;
+    cout << "\t\t\t\t4. Sort employee list" << endl;
+    cout << "\t\t\t\t5. Save to file" << endl;
     cout << "\t\t\t\t6. Exit" << endl;
 }
 void menuOption() {
-    cout << "\t\t\t\t  Enter your choice:> "; cin >> mo;
+    cout << "\t\t\t\t  Enter your choice: "; cin >> option;
 
-    switch(mo) {
+    switch(option) {
         case 1:
             system("cls");
-            emps.enterEmpList();
+            emps.enterEmps();
             writeToInputFile(emps);
-            if(returnToMenu()) {
+            if(returnMenu()) {
                 system("cls");
                 showMenu();
                 menuOption();
@@ -145,16 +189,15 @@ void menuOption() {
             empF = emps.findEmp();
             if(empF.empCode) {
                 cout << "======================" << endl;
-                cout << "Employee code: " << empF.empCode;
-                cout << "\nFirst name: " << empF.firstName << endl;
-                cout << "\nLast name: " << empF.lastName << endl;
-                cout << "\nSalary: " << empF.salary << endl;
-                cout << "======================" << endl;
+                cout << "Employee code: " << empF.empCode << endl;
+                cout << "First name: " << empF.firstName << endl;
+                cout << "Last name: " << empF.lastName << endl;
+                cout << "Salary: " << empF.salary << endl;
             } else {
                 cout << "======================" << endl;
                 cout << "Not found" << endl;
             }
-            if(returnToMenu()) {
+            if(returnMenu()) {
                 system("cls");
                 showMenu();
                 menuOption();
@@ -168,16 +211,15 @@ void menuOption() {
             if(empS.empCode) {
                 // block empty list
                 cout << "======================" << endl;
-                cout << "Employee code: " << empS.empCode;
-                cout << "\nFirst name: " << empS.firstName << endl;
-                cout << "\nLast name: " << empS.lastName << endl;
-                cout << "\nSalary: " << empS.salary << endl;
-                cout << "======================" << endl;
+                cout << "Employee code: " << empS.empCode << endl;
+                cout << "First name: " << empS.firstName << endl;
+                cout << "Last name: " << empS.lastName << endl;
+                cout << "Salary: " << empS.salary << endl;
             } else {
                 cout << "======================" << endl;
                 cout << "Employees is empty" << endl;
             }
-            if(returnToMenu()) {
+            if(returnMenu()) {
                 system("cls");
                 showMenu();
                 menuOption();
@@ -190,7 +232,7 @@ void menuOption() {
             emps.sort();
             cout << "After sorted" << endl;
             emps.showEmpList();
-            if(returnToMenu()) {
+            if(returnMenu()) {
                 system("cls");
                 showMenu();
                 menuOption();
@@ -199,8 +241,9 @@ void menuOption() {
             }
             break;
         case 5:
+            system("cls");
             writeToOutputFile(empF, empS, emps);
-            if(returnToMenu()) {
+            if(returnMenu()) {
                 system("cls");
                 showMenu();
                 menuOption();
@@ -209,7 +252,7 @@ void menuOption() {
             }
             break;
         case 6:
-            cout << "\t\t\t\t  Exited" << endl;
+            cout << "\t\t\t\t  Exited";
             break;
         default:
             system("cls");
@@ -219,67 +262,84 @@ void menuOption() {
     }
 }
 void writeToInputFile(empList emps) {
-    fstream myfile;
-    myfile.open("input3.txt", ios::app);
-    int len = emps.ds.size();
-    if(len) {
-        for(int i = 0; i < len; i++) {
-            myfile << ds[i].empCode << endl;
-            myfile << ds[i].firstName << endl;
-            myfile << ds[i].lastName << endl;
-            myfile << ds[i].salary << endl;
-        }
+    ofstream myfile;
+    myfile.open("input3.txt");
+    for(int i = 0; i < emps.n; i++) {
+        myfile << emps.ds[i].empCode << endl;
+        myfile << emps.ds[i].lastName << endl;
+        myfile << emps.ds[i].firstName << endl;
+        myfile << emps.ds[i].salary << endl;
     }
     myfile.close();
-    cout << "\nWrited to input1.txt\n\n";
+    cout << "Writed to file" << endl;
 }
 void writeToOutputFile(Employee empF, Employee empS, empList emps) {
-    fstream myfile;
-    myfile.open("output3.txt", ios::app);
+    ofstream myfile;
+    myfile.open("output3.txt");
 
+    myfile << "======================" << endl;
     myfile << "Find employees result: " << endl;
+    cout << "======================" << endl;
+    cout << "Find employees result: " << endl;
     if(empF.empCode) {
-        myfile << "Employee code: " << empF.empCode;
-        myfile << "\nFirst name: " << empF.firstName << endl;
-        myfile << "\nLast name: " << empF.lastName << endl;
-        myfile << "\nSalary: " << empF.salary << endl;
-        myfile << "======================" << endl;
+        myfile << "Employee code: " << empF.empCode << endl;
+        myfile << "First name: " << empF.firstName << endl;
+        myfile << "Last name: " << empF.lastName << endl;
+        myfile << "Salary: " << empF.salary << endl;
+        cout << "Employee code: " << empF.empCode << endl;
+        cout << "First name: " << empF.firstName << endl;
+        cout << "Last name: " << empF.lastName << endl;
+        cout << "Salary: " << empF.salary << endl;
     } else {
-        myfile << "Not found\n\n";
+        myfile << "Not found" << endl;
+        cout << "Not found" << endl;
     }
 
+    myfile << "======================" << endl;
     myfile << "Max of salary result: " << endl;
+    cout << "======================" << endl;
+    cout << "Max of salary result: " << endl;
     if(empS.empCode) {
         // block empty list
-        myfile << "Employee code: " << empS.empCode;
-        myfile << "\nFirst name: " << empS.firstName << endl;
-        myfile << "\nLast name: " << empS.lastName << endl;
-        myfile << "\nSalary: " << empS.salary << endl;
-        myfile << "======================" << endl;
+        myfile << "Employee code: " << empS.empCode << endl;
+        myfile << "First name: " << empS.firstName << endl;
+        myfile << "Last name: " << empS.lastName << endl;
+        myfile << "Salary: " << empS.salary << endl;
+        cout << "Employee code: " << empS.empCode << endl;
+        cout << "First name: " << empS.firstName << endl;
+        cout << "Last name: " << empS.lastName << endl;
+        cout << "Salary: " << empS.salary << endl;
     } else {
-        myfile << "Employees is empty\n\n";
+        myfile << "Employees is empty" << endl;
+        cout << "Employees is empty" << endl;
     }
+    myfile << "======================" << endl;
     myfile << "Employees after sorted: " << endl;
+    cout << "======================" << endl;
+    cout << "Employees after sorted: " << endl;
     int tmp = emps.ds.size();
     if(tmp) {
         for(int i = 0; i < tmp; i++) {
-            myfile << "======================" << endl;
-            myfile << "Employee code: " << emps.ds[i].empCode;
-            myfile << "\nFirst name: " << emps.ds[i].firstName << endl;
-            myfile << "\nLast name: " << emps.ds[i].lastName << endl;
-            myfile << "\nSalary: " << emps.ds[i].salary << endl;
+            myfile << "Employee code: " << emps.ds[i].empCode << endl;
+            myfile << "First name: " << emps.ds[i].firstName << endl;
+            myfile << "Last name: " << emps.ds[i].lastName << endl;
+            myfile << "Salary: " << emps.ds[i].salary << endl;
+            cout << "Employee code: " << emps.ds[i].empCode << endl;
+            cout << "First name: " << emps.ds[i].firstName << endl;
+            cout << "Last name: " << emps.ds[i].lastName << endl;
+            cout << "Salary: " << emps.ds[i].salary << endl;
         }
     } else {
-        myfile << "Employees is empty\n\n";
+        myfile << "Employees is empty" << endl;
+        cout << "Employees is empty" << endl;
     }
 
     myfile.close();
-    cout << "\nWrited to output1.txt\n\n";
+    cout << endl << "Writed to output1.txt" << endl;
 }
-bool returnToMenu() {
-    cout << "Enter H to return to the menu:> "; cin >> rtnMenu;
-
-    if(rtnMenu == 'H' || rtnMenu == 'h') {
+bool returnMenu() {
+    cout << "Enter H to return to the menu:> "; cin >> rtnmenu;
+    if(rtnmenu == 'h' || rtnmenu == 'H') {
         return true;
     } return false;
 }
