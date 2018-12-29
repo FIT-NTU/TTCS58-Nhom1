14b86a5eba0733bfb6e884eaca79f205ce5a5f2d
thanhlong <thanhlong98kun@gmail.com>
2018-12-29 12:20:06 +0700

    Sua bai 3 lan cuoi

--- Bai_3/main.cpp
+++ Bai_3/main.cpp
@@ -6,10 +6,15 @@
 #include <iomanip>
 using namespace std;
 
-#define N 4
+#define N 50
 #define numWidth 10
 #define nameWidth 30
 #define separator ' '
+#define INPUT "input3.txt"
+#define OUTPUT "output3.txt"
+
+//ofstream write;
+//ifstream read;
 struct emp {
 	int id;
 	string lastname;
@@ -18,49 +23,54 @@ struct emp {
 };
 typedef struct emp Emp;
 
+// CAC HAM CHUC NANG
 void readFile(vector<Emp> &list, string path) {
 	int n;
-	ifstream file;
 	Emp temp;
+	ifstream read;
 	list.clear();
-	file.open(path.c_str(), ios::out);
-	if (file.is_open()) {
-		file >> n;
+	read.open(path.c_str(), ios::out);
+	if (read.is_open()) {
+		read >> n;
 		if (n > 50) {
 			cout << "So nhan vien qua 50" << endl;
-			file.close();
+			read.close();
 			return;
 		}
 		for (int i = 0; i < n; i++) {
-			file >> temp.id;
-			file >> temp.lastname;
-			file >> temp.firstname;
-			file >> temp.salary;
+			read >> temp.id;
+			read >> temp.lastname;
+			read >> temp.firstname;
+			read >> temp.salary;
 			list.push_back(temp);
 		}
 	}
-
-	file.close();
+	read.close();
 }
 
 void writeFile(string * s, int n, string path) {
-	ofstream file;
-	file.open(path.c_str(), ios_base::out);
-
+	ofstream write;
+	write.open(path.c_str(), ios_base::out);
 	for (int row = 0; row < n; row++) {
-		file << "Emp: " << row + 1 << endl;
+		write << "Emp: " << row + 1 << endl;
 		for (int col = 0; col < N; col++) {
-			file << s[row  * N + col] << endl;
+			write << s[row  * N + col] << endl;
 		}
 	}
-	file.close();
+	write.close();
 }
 
-
-
+void writeStringContinue(string s, string path) {
+	ofstream write;
+	write.open(path.c_str(), ios::out | ios::app);
+	if (write.is_open()) {
+		write << s << endl;
+	}
+	write.close();
+}
 
 // tra ve nhan vien co ma nhan vien
-int searchEmp(vector<Emp> list, int id) {
+int checkEmp(vector<Emp> list, int id) {
 	int n = list.size();
 	Emp * p = new Emp;
 	for (int i = 0; i < n; i++) {
@@ -71,8 +81,8 @@ int searchEmp(vector<Emp> list, int id) {
 	return -1;
 }
 
-// Tra ve luong cao nhat hoac thap nhat
-int Salary(vector<Emp> list, int k) {
+// Tra ve luong cao nhat hoac thap nhat tuy thuoc vao so k
+int salaryHL(vector<Emp> list, int k) {
 	int n = list.size();
 	int minmax = list.at(0).salary;
 
@@ -91,6 +101,10 @@ int Salary(vector<Emp> list, int k) {
 	}
 	return minmax;
 }
+
+
+// HAM XY LY
+/* ================================================================ */
 
 // Sap xep nhan vien
 void sortName(vector<Emp> &list) {
@@ -113,16 +127,6 @@ void sortName(vector<Emp> &list) {
 
 		}
 	}
-	cout << "Da sap xep thanh cong" << endl;
-}
-
-void menu() {
-	cout << "1. Nhap thong tin nhan vien" << endl;
-	cout << "2. Thong tin nhan vien tu ma nhan vien" << endl;
-	cout << "3. Nhan vien co luong cao nhat va thap nhat" << endl;
-	cout << "4. Sap xep nhan vien theo ten" << endl;
-	cout << "5. Hien tat ca nhan vien" << endl;
-	cout << "6. Thoat" << endl;
 }
 
 // In ra tat ca cac nhan vien
@@ -145,15 +149,30 @@ void showAllEmp(vector<Emp> list) {
 	cout << endl;
 }
 
-// In ra danh sach nhan vien co dieu kien
-void showSelectEmp(vector<Emp> list, int * c, int n) {
+int * listEmpSalary(vector<Emp> list, int k) {
+	int n = list.size();
+	int * a = new int[n], index = 0;
+	int salary = salaryHL(list, k);
+	for (int i = 0; i < n; i++) {
+		if (list.at(i).salary == salary) {
+			index = index + 1;
+			a[index] = i;
+		}
+	}
+	a[0] = index;
+	return a;
+}
+
+// In ra danh sach nhan vien co co trong mang c, chi so dau tien la so luong
+void showEmpSelect(vector<Emp> list, int * c) {
+	int n = c[0];
 	cout << left << setw(numWidth) << setfill(separator) << "ID";
 	cout << left << setw(nameWidth) << setfill(separator) << "Ho";
 	cout << left << setw(nameWidth) << setfill(separator) << "Ten";
 	cout << left << setw(numWidth) << setfill(separator) << "Luong";
 	cout << endl;
 
-	for (int i = 0; i < n; i++) {
+	for (int i = 1; i <= n; i++) {
 		cout << left << setw(numWidth) << setfill(separator) << list.at(c[i]).id;
 		cout << left << setw(nameWidth) << setfill(separator) << list.at(c[i]).lastname;
 		cout << left << setw(nameWidth) << setfill(separator) << list.at(c[i]).firstname;
@@ -163,56 +182,54 @@ void showSelectEmp(vector<Emp> list, int * c, int n) {
 	cout << endl;
 }
 
-// In ra nhan vien co luong cao nhat hoac thap nhat
-void showSalary(vector<Emp> list, int size) {
-	int * c = new int[size], n = 0;
-	int temp;
-	cout << "Cao nhat (0) hay thap nhat (1): ";
-	do {
-		cin >> temp;
-		if (!(temp == 1 || temp == 0)) {
-			cout << "Nhap lai: ";
-		}
-	} while (!(temp == 1 || temp == 0));
-	system("cls");
+void writeEmpSelect(vector<Emp> list, int * c, string path) {
+	int n = c[0];
+	ofstream write;
 
-	cout << "Nhan vien co luong " << (temp == 0 ? "cao nhat" : "thap nhat") << " la: " << endl;
-	temp = Salary(list, temp);
-	for (int i = 0; i < size; i++) {
-		if (list.at(i).salary == temp) {
-			c[n++] = i;
+	write.open(path.c_str(), ios::out | ios::app);
+	if (write.is_open()) {
+		write << left << setw(numWidth) << setfill(separator) << "ID";
+		write << left << setw(nameWidth) << setfill(separator) << "Ho";
+		write << left << setw(nameWidth) << setfill(separator) << "Ten";
+		write << left << setw(numWidth) << setfill(separator) << "Luong";
+		write << endl;
+
+		for (int i = 1; i <= n; i++) {
+			write << left << setw(numWidth) << setfill(separator) << list.at(c[i]).id;
+			write << left << setw(nameWidth) << setfill(separator) << list.at(c[i]).lastname;
+			write << left << setw(nameWidth) << setfill(separator) << list.at(c[i]).firstname;
+			write << left << setw(numWidth) << setfill(separator) << list.at(c[i]).salary;
+			write << endl;
 		}
+		write << endl;
 	}
-	showSelectEmp(list, c, n);
-	delete[] c;
+
+	write.close();
 }
 
-// In ra nhan vien theo ma nhan vien
-void showEmpId(vector<Emp> list, int n) {
-	if (searchEmp(list, n) != -1) {
-		int * a = new int;
-		a[0] = searchEmp(list, n);
-		showSelectEmp(list, a, 1);
-		delete[] a;
-	}
-	else {
-		cout << "Khong co ma so nhan vien nay!!!" << endl;
-	}
+void menu() {
+	cout << "1. Nhap thong tin nhan vien" << endl;
+	cout << "2. Thong tin nhan vien tu ma nhan vien" << endl;
+	cout << "3. Nhan vien co luong cao nhat va thap nhat" << endl;
+	cout << "4. Sap xep nhan vien theo ten" << endl;
+	cout << "5. Hien tat ca nhan vien" << endl;
+	cout << "6. Thoat" << endl;
 }
 
 void select(vector<Emp> &list) {
-	int n = 0, temp = 0, size = list.size();
+	int n = 0, temp = 0, *tempA;
+
 	cout << "Ban chon: "; cin >> n;
 	if (n == 6) {
 		return;
 	}
-	fstream file;
-	file.open("output3.txt", ios::out | ios::app);
+
 	switch (n)
 	{
-
 	case 1:
 		readFile(list, "input3.txt");
+		writeStringContinue("=======================================================================", OUTPUT);
+
 		system("cls");
 		cout << "Nhap thanh cong!" << endl;
 		menu();
@@ -221,41 +238,61 @@ void select(vector<Emp> &list) {
 	case 2:			// Tim nhan vien theo ma nhan vien
 		cout << "Ma nhan vien: "; cin >> temp;
 		system("cls");
-		showEmpId(list, temp);
-		if (file.is_open()) {
-			file << "Thong tin nhan vien co ma la " << list.at(temp).id << endl;
-			file << "Ten: " << list.at(temp).lastname << " " << list.at(temp).firstname << endl;
-			file << "Luong: " << list.at(temp).salary << endl;
+		if (checkEmp(list, temp) != -1) {
+			tempA = new int[2];
+			tempA[0] = 1;
+			tempA[1] = checkEmp(list, temp);
+			showEmpSelect(list, tempA);
+			writeStringContinue("Ban Chon 2: Thong tin nhan vien tu ma nhan vien", OUTPUT);
+			writeEmpSelect(list, tempA, "output3.txt");
 		}
+		else {
+			cout << "Khong co nhan vien co ma la " << temp << endl;
+		}
+
 		menu();
 		select(list);
 		break;
 
 	case 3:	// In ra nhan vien co luong cao nhat hoac thap nhat
-		
-		showSalary(list, size);
+		do {
+			cout << "Cao nhat (0) - thap nhat(1):"; cin >> temp;
+		} while (temp != 0 && temp != 1);
+		system("cls");
+		tempA = new int[N];
+		tempA = listEmpSalary(list, temp);
+		writeStringContinue("Ban chon 3: Tim nhan vien co luong cao nhat, thap nhat", OUTPUT);
+		if (temp == 0) {
+			cout << "Nhan vien co luong cao nhat la:" << endl;
+			writeStringContinue("Nhan vien co luong cao nhat la:", OUTPUT);
+		}
+		else {
+			cout << "Nhan vien co luong thap nhat la:" << endl;
+			writeStringContinue("Nhan vien co luong cao nhat la:", OUTPUT);
+		}
+		showEmpSelect(list, tempA);
+		writeEmpSelect(list, tempA, OUTPUT);
+
 		menu();
 		select(list);
 		break;
 	case 4:
-		sortName(list);
 		system("cls");
+		sortName(list);
 		showAllEmp(list);
+		writeStringContinue("Ban chon 4: Sap xep nhan vien\nSap xep nhan vien thanh cong", OUTPUT);
 		menu();
 		select(list);
-		if (file.is_open()) {
-			file << "Da sap xep thanh cong" << endl;
-		}
 		break;
 	case 5:
 		system("cls");
 		showAllEmp(list);
 		menu();
+		writeStringContinue("Ban chon 5: Hien tat ca nhan vien co trong danh sach", OUTPUT);
 		select(list);
 		break;
 	case 6:
-		file.close();
-		break;
+		return;
 	default:
 		system("cls");
 		cout << "Nhap sai" << endl;
@@ -266,7 +303,7 @@ void select(vector<Emp> &list) {
 }
 
 int main() {
-	int n = 0;
+	int n = 0, *a = new int[N];
 	vector<Emp> dsNV;
 	menu();
 	select(dsNV);
